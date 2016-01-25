12689cbdcec2dd74cfb120d93165c4d9cb98b12b
narv1 <narv@onders.org>
2016-01-25 18:21:59 +0100

    -e fertig

--- adresse.c
+++ adresse.c
@@ -20,17 +20,19 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 #include <unistd.h>
 #include "myString.h"
 #define N 50
 
+
+
 int main(int argc, char *argv[])
 {
   int stderr = 0;
   int opt;
-  int i, j;
-  char vorn1[N], vorn2[N], nachn[N], stra[N], plz[N], ort[N], land[N], gesamt[100];
+  char vorn1[N], test[N], vorn2[N], nachn[N], stra[N], plz[N], ort[N], land[N], gesamt[100];
   extern char *optarg;
   extern int optind, optopt;
   
@@ -38,12 +40,6 @@ int main(int argc, char *argv[])
     switch (opt) {
     case 'h':
       printf("Dieses Programm hilft Adressen eines Kundestockes X zuverwalten, die Eingabe ist wieder mit getopt gestaltet\n\nBeschreibung:\n\t-e\t\"...\"\tEingabe der Adresse erfolgt nacheinander. Reihenfolge: Vorname1, Vorname2, Nachname, Strasze, Postleitzahl, Ort, Land.\n\t-g\t\"...\"\tEingabe der Adresse erfolgt in einem Satz es ist zu beachten:\n\t\t\t1) Das die Wörter entweder mit einem \"Space\" oder mit einem \",\" zu trennen sind\n.\t\t\t.2) Das \"unbedingt\" die Reihenfolge einzuhalten ist.\n\n\t-h\t\"...\"\tBei Programm aufruf mit \"-h\" wird die Hilfe des Programms ausgegeben.\n");
-      stderr++;
-      break;
-    case ':':
-      if (optopt == 'c'){
-	printf ("Option -%c requires an argument.\n", optopt);
-      }
       stderr++;
       break;
     case'?':
@@ -54,6 +50,7 @@ int main(int argc, char *argv[])
       break;
     case 'e':
       stderr++;
+
       printf("Bitte geben sie den Vornamen ein:\n(Falls vorhanden mit Leerezeichen oder \",\" den 2.Vornamen dahinter):\n");
       fgets(vorn1, N, stdin);
       printf("Bitte geben sie ihren Nachnamen ein:\n");
@@ -67,58 +64,95 @@ int main(int argc, char *argv[])
       printf("Bitte geben sie ihr Land ein:\n");
       fgets(land, N, stdin);
 
-      int i, j, l = 0,  k = 0;
+      int i, j, k = 0,  l = 0, m = 0;
+      
+      // Zur überprüfung der kommenden if-abfrage auf mehrere Vornamen
       k = strlength(vorn1, N);
+      char lookup[] = " ,";
+      l = strcspn(vorn1,lookup);
+	
       for(i = 0; i < k; i++){
-	if (vorn1[i] == 32 || vorn1[i] == 44){
+	if(l  == i){
 	  vorn1[i] = '\0';
 	  i++;
+	  m = 0;
 	  for( ; i < k; i++){
 	    vorn2[j] = vorn1[i];
 	    j++;
-	    if(k == i+1){
-	      vorn2[j-1] = '\0';
-	      l++;
-	      break;
+	    if(k - 1 == i){
+	      vorn2[j - 1] = '\0'; // -1
+	      m = 1;
 	    }
 	  }
 	}
       }
-	
-      i = strlength(vorn2, N);
-      k = strlength(vorn1, N);
-      printf("\nVorname1: %s  länge: %d", vorn1, k);
-      printf("\nVorname2: %s  länge; %d\n", vorn2, i);
-
-      if(k > i){
-	j = strsearch(vorn2, N, vorn1);
-      }else{
-	j = strsearch(vorn1, N, vorn2);
+      if(m == 0){
+	i = strlength(vorn1, N);
+	vorn1[i - 1] = '\0';
       }
-      
-      printf("\n%d",j);	  
-      
-      if(l == 0){
-	printf("\nhallo\n");
-	break;
+      //Abfrage + Ausgabe + Verlgiech (1Vor- und 2Vornamen)
+      if (m == 1){
+	i = strlength(vorn1, N);
+	k = strlength(vorn2, N);
+	if(k <= i){
+	  printf("\nVorname1:\t%d\t%s", i, vorn1);
+	  printf("\nVorname2:\t%d\t%s\n", k, vorn2);
+	  j = strsearch(vorn2, N, vorn1);
+	}
+	if(k > i){
+	  printf("\nVorname1:\t%d\t%s", i, vorn1);
+	  printf("\nVorname2:\t%d\t%s\n", k, vorn2);
+	  j = strsearch(vorn1, N, vorn2);
+	}
+	if(j == -1){
+	  printf("Die Woerter sind sich nicht aehnlich.\n");
+	  m = 0;
+	}else{
+	  printf("Die Woerter sind sich aehnlich.\n");
+	  m = 0;
+	}
       }
-      
+      //Abfrage + Ausgabe + Verlgiech (Vor- und Nachnamen)
+      if(m == 0){
+	i = 0;
+	i = strlength(vorn1, N);
+	vorn1[i] = '\0';
+	k = strlength(nachn, N);
+	nachn[k - 1] = '\0';
+	j = 0;
+	if(k <= i){
+	  printf("\nVorname1:\t%d\t%s", i, vorn1);
+	  printf("\nNachname:\t%d\t%s\n", k - 1, nachn);
+	  j = strsearch(nachn, N, vorn1);
+	}
+	if(k > i){
+	  printf("\nVorname1:\t%d\t%s", i, vorn1);
+	  printf("\nNachname:\t%d\t%s\n", k - 1, nachn);
+	  j = strsearch(vorn1, N, nachn);
+	}
+	if(j == -1){
+	  printf("Die Wörter sind sich nicht aehnlich.\n");
+	}else{
+	  printf("Die Wörter sind sich aehnlich.\n");
+	}
+      }
+      //Ausgabe
       i = strlength(stra, N);
-      stra[i] = '\0';
-      printf("\nStraße:\t\t%d\t%s\n", i, stra);
+      stra[i - 1] = '\0';
+      printf("\nStraße:\t\t%d\t%s", i - 1, stra);
       
       i = strlength(plz, N);
-      plz[i] = '\0';
-      printf("\nPLZ:\t\t%d\t%s\n", i, plz);
+      plz[i - 1] = '\0';
+      printf("\nPLZ:\t\t%d\t%s", i - 1, plz);
       
       i = strlength(ort, N);
-      ort[i] = '\0';
-      printf("\nOrt:\t\t%d\t%s\n", i, ort);
+      ort[i - 1] = '\0';
+      printf("\nOrt:\t\t%d\t%s", i - 1, ort);
       
       i = strlength(land, N);
-      land[i] = '\0';
-      printf("\nLand:\t\t%d\t%s\n", i, land);
-      break;
+      land[i - 1] = '\0';
+      printf("\nLand:\t\t%d\t%s\n", i - 1, land);
+      return 0;
       
   /******************************/
   /* Aufruf des Programms mit   */
@@ -126,15 +160,19 @@ int main(int argc, char *argv[])
   /*           String           */
   /******************************/
     case 'g':
-      printf("Bitte geben sie die gesamte Adresse ein:\n/Formatierung: Vorname1,Vorname2,Nachname,Strasze,Postleitzahl,");
+      stderr++;
+      printf("Bitte geben sie die gesamte Adresse ein:\n/Formatierung: Vorname1,Vorname2,Nachname,Strasze,Postleitzahl,Ort,Land\n");
       fgets(gesamt, 100, stdin);
     
       if(stderr == 0){
-	printf("This program, requires an argument.\nPls type %s -h for more information.\n",argv[0]);
+	printf("This program, requires an argument.\nPlease type %s -h for more information.\n",argv[0]);
 	break;
       }
     }
     return 0;
+  }
+  if(stderr == 0){
+    printf("This program, requires an argument.\nPlease type %s -h for more information.\n",argv[0]);
   }
   return 0;
 }
